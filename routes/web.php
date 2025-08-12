3845addb44a72ca8aa6e5970bf27d370dd66147f
Octavio Solorzano Roblero <octaviosolorzano91@gmail.com>
2025-08-12 10:01:01 -0600

    Rutas para las nuevas vistas

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PagesController;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/quienes-somos', [PagesController::class, 'about'])->name('about');
+Route::get('/faq',            [PagesController::class, 'faq'])->name('faq');
+Route::get('/mapa',           [PagesController::class, 'map'])->name('map');
+Route::get('/familias-reunidas', [PagesController::class, 'reunions'])->name('reunions');
+Route::get('/precios',        [PagesController::class, 'pricing'])->name('pricing');
